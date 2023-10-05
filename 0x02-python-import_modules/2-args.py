43d136a66506b1dee9753068f348d718c6b468b7
ahmed <ahelmyeng1@gmail.com>
2023-10-05 03:13:08 -0700

    task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(argv) == 2:
         args = argv[1:]
-        print(f"{len(args):d} argument:")
+        print("{:d} argument:".format(len(args)))
         for item, val in enumerate(args, start=1):
-            print(f"{item:d}: {val}")
+            print("{:d}: {}".format(item, val))
     else:
         args = argv[1:]
-        print(f"{len(args):d} arguments:")
+        print("{:d} arguments:".format(len(args)))
         for item, val in enumerate(args, start=1):
-            print(f"{item:d}: {val}")
+            print("{:d}: {}".format(item, val))
