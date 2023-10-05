36d2b1acb333336bb357d76d2a9fcf952309057e
ahmed <ahelmyeng1@gmail.com>
2023-10-05 09:02:40 -0700

    task

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-from calculator_1 import add, sub
+import importlib
 
 
 def magic_calculation(a, b):
     '''calculate sub and add'''
     if a < b:
+        add = importlib.import_module("magic_calculation_102").add
         c = add(a, b)
-        for i in range(4, 6):
+        for i in range(4, 7):
             c = add(c, i)
         return c
     else:
-        return sub(a, b)
+        return importlib.import_module("magic_calculation_102").sub(a, b)
