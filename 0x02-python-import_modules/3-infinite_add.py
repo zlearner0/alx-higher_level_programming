add349fa1a001e303d21f743dbc795c9904919d1
ahmed <ahelmyeng1@gmail.com>
2023-10-05 09:53:19 -0700

    task

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     from sys import argv
-    list_len = len(argv)
-    if list_len == 1:
-        print(0)
-    else:
-        my_args = argv[1:]
-        total_sum = 0
-        for item in my_args:
-            total_sum += int(item)
+    my_args = argv[1:]
+    total_sum = 0
+    for item in my_args:
+        total_sum += int(item)
 print(total_sum)
