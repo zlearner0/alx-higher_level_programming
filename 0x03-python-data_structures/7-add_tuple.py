0d2b976d0a50b0ca61672b353b26c8f4a7e01077
ahmed <ahelmyeng1@gmail.com>
2023-10-06 23:40:48 -0700

    task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     '''adding 2 tuples elements'''
-    if tuple_a == ():
-        return tuple_b
-    elif tuple_b == ():
-        return tuple_a
-    x = tuple_a[0] + tuple_b[0]
-    if len(tuple_a) == 1 and len(tuple_b) > 1:
-        y = tuple_b[1]
-    if len(tuple_b) == 1 and len(tuple_a) > 1:
-        y = tuple_a[1]
-    if len(tuple_a) > 1 and len(tuple_b) > 1:
-        y = tuple_a[1] + tuple_b[1]
-    return x, y
+    a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+    b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+
+    a2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    sum1 = a1 + b1
+    sum2 = a2 + b2
+
+    return (sum1, sum2)
