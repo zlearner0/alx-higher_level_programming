36b3557def462bbab2cb9165c25c7b7e159feffb
ahmed <ahelmyeng1@gmail.com>
2023-10-06 23:20:17 -0700

    task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     '''print list elements separately'''
     if not matrix:
         return None
-    else:
-        for small_list in matrix:
-            for i in small_list:
-                print("{:d} ".format(i), end="" if small_list.index(
-                    i) < len(small_list) - 1 else "\n")
+    for small_list in matrix:
+        for i in small_list:
+            print("{:d} ".format(i), end=" " if small_list.index(
+                i) < len(small_list) - 1 else "\n")
