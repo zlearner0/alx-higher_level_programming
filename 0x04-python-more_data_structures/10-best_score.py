c68937f2035fe5c4ba3e426c33d46e8a209c67c1
ahmed <ahelmyeng1@gmail.com>
2023-10-11 10:31:33 -0700

    task

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,8 +3,4 @@
 
 def best_score(a_dictionary):
     '''give max value of a dictionary'''
-    if (not a_dictionary.values()):
-        return None
-    if (not a_dictionary):
-        return None
     return max(a_dictionary.values())
