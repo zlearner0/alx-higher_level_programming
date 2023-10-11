f35b048f6ea121904323682de63fea1ac837fb61
ahmed <ahelmyeng1@gmail.com>
2023-10-11 10:14:21 -0700

    task

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,6 +2,8 @@
 def uniq_add(my_list=[]):
     if (my_list is None):
         return None
+    if (my_list == []):
+        return 0
     '''returns a unique elements list'''
     my_set = set(my_list)
     return sum(my_set)
