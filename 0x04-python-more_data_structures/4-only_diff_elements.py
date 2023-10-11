edc9733b1f855475c66ec2676e3356f93338ac5d
ahmed <ahelmyeng1@gmail.com>
2023-10-11 10:23:52 -0700

    task

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,10 +3,4 @@
 
 def only_diff_elements(set_1, set_2):
     '''returns a set of the different of 2 sets'''
-    if (not set_1):
-        return set_2
-    if (not set_2):
-        return
-    if (not set_1 and not set_2):
-        return None
     return set_1.symmetric_difference(set_2)
