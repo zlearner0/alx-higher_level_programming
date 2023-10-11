1b5505a81f176946245ca8e6595f45503c2cc168
ahmed <ahelmyeng1@gmail.com>
2023-10-11 09:28:38 -0700

    task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,10 @@ def search_replace(my_list, search, replace):
     '''returns a list with replaced elements'''
     if (not my_list):
         return None
+    new_list = []
     for item in my_list:
         if item == search:
             my_list[my_list.index(item)] = replace
-    return my_list
+        else:
+            new_list.append(item)
+    return new_list
