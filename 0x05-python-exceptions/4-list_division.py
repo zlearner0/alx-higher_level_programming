3ea0ef9e0f275ac7638445ea12b3706a14a9c744
ahmed <ahelmyeng1@gmail.com>
2023-10-23 09:31:31 -0700

    task

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,12 @@
+#!/usr/bin/python3
+
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
     for i in range(list_length):
         try:
             result = 0
-            if type(my_list_1[i]) == int or         type(my_list_1[i]) == float:
+            if type(my_list_1[i]) == int or type(my_list_1[i]) == float:
                 if type(my_list_2[i]) == int or type(my_list_2[i]) == float:
                     result = my_list_1[i] / my_list_2[i]
                 else:
@@ -17,3 +20,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(result)
     return new_list
+
