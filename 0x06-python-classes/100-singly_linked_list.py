a1693a0ce5634990e0db6c02dddf758287c46742
ahmed <ahelmyeng1@gmail.com>
2023-10-24 11:24:54 -0700

    task

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -17,98 +17,95 @@ class Node:
             data (int): the node data
             next_node (Node): the next node in the linked list
         """
-        self.data = data
-        self.next_node = next_node
+        self.__data = data
+        self.__next_node = next_node
 
     @property
     def data(self):
-        """
-        Returns:
-            The node data
-        """
+        '''
+        getting the data attribute
+        '''
         return self.__data
 
     @data.setter
     def data(self, value):
-        """
-        Sets the node data
-
-        Args:
-            value (int): the node data
-
-        Raises:
-            TypeError: if value isn't an integer
-        """
+        '''
+        setting data attribute
+        Parameters:
+        - value: data input from outside class
+        Raise:
+        - TypeError: if data input is not integer
+        '''
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """
-        Returns:
-            The next node in the linked list
-        """
+        '''
+        getting the next_node attribute
+        '''
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """
-        Sets the next node in the linked list
-
-        Args:
-            value (Node): the next node in the linked list
-
-        Raises:
-            TypeError: if value isn't None or a Node object
-        """
-        if value is not None and not isinstance(value, Node):
+        '''
+        setting next_node attribute
+        Parameters:
+        - value: data input from outside class
+        Raise:
+        - TypeError: if data input is object
+        '''
+        if value is None or isinstance(value, Node):
+            self.__next_node = value
+        else:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = value
 
 
 class SinglyLinkedList:
-    """
-    This class defines a singly linked list
-    """
+    '''
+    This is the documentation for SinglyLinkedList class.
+    class creates a singly linked list
+    '''
 
     def __init__(self):
         """
-        Initializes a new, empty singly linked list
+        Initializes a new Node object
         """
-        self.head = None
+        self.__head = None
 
     def sorted_insert(self, value):
-        """
-        Inserts a new Node into the correct sorted position in the linked list
+        '''
+        sorting the nodes upon their value ascendingly
+        - value: the data integer of the node
+        '''
+        current = Node(value)
 
-        Args:
-            value (int): the value to insert into the linked list
-        """
-        new_node = Node(value)
-
-        if self.head is None:
-            self.head = new_node
-        elif value < self.head.data:
-            new_node.next_node = self.head
-            self.head = new_node
+        if self.__head is None:
+            self.__head = current
+        elif current.data <= self.__head.data:
+            current.next_node = self.__head
+            self.__head = current
         else:
-            crnt = self.head
-            while crnt.next_node is not None and crnt.next_node.data < value:
-                crnt = crnt.next_node
-            new_node.next_node = crnt.next_node
-            crnt.next_node = new_node
+            h = self.__head
+            while (h):
+                if h.next_node and h.next_node.data > current.data:
+                    current.next_node = h.next_node
+                    h.next_node = current
+                    break
+                elif not h.next_node:
+                    h.next_node = current
+                    break
+
+                h = h.next_node
 
     def __str__(self):
-        """
-        Returns:
-            A string representation of the linked list
-        """
-        result = ""
-
-        current = self.head
-        while current is not None:
-            result += str(current.data) + "\n"
-            current = current.next_node
-
-        return result
+        '''
+        string method prints the nodes data
+        '''
+        h = self.__head
+        res = ''
+        while (h):
+            res += str(h.data) + '\n'
+            h = h.next_node
+        return res
