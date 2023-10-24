015571fcd0eb971d339479f9b0f8a79a5926aaea
ahmed <ahelmyeng1@gmail.com>
2023-10-24 08:47:46 -0700

    task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the documentation for the module 0-square.py.
+"""
+
+
+class Square:
+    """
+    This is the documentation for the Square class.
+    It represents a square shape with a given size.
+    """
+    pass
