506df85bbdd303f9d191c2db7be8d6dfdc21eb24
ahmed <ahelmyeng1@gmail.com>
2023-10-24 08:49:03 -0700

    task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is the documentation for the module 4-square.py.
+"""
+
+
+class Square:
+    """
+    This is the documentation for the Square class.
+    It represents a square shape with a given size.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square object.
+
+        Parameters:
+        - size (int): the size of the square (default 0)
+        """
+        self.__size = size
