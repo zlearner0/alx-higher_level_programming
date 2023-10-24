f8f0a56da2b08e5930bd92b6bbd6e393d0943933
ahmed <ahelmyeng1@gmail.com>
2023-10-24 08:51:39 -0700

    task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This is the documentation for the module 4-square.py.
+"""
+
+
+class Square:
+    """
+    This is the documentation for the Square class.
+    It represents a square shape with a given size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square object.
+
+        Parameters:
+        - size (int): the size of the square (default 0)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+        - the area of the square (int)
+        """
+        return self.__size**2
