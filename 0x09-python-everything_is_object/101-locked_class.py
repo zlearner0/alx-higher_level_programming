5354b4833b1f075d22e314f9542c7d0830ea2ed8
ahmed <ahelmyeng1@gmail.com>
2023-10-31 06:21:33 -0700

    task

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,9 +2,13 @@
 
 
 class LockedClass:
-    '''creates only protected instance attributes'''
+    """prevents the user from dynamically creating instance attributes."""
     __slots__ = ['first_name']
 
-    def __init__(self):
-        '''sets the instance attribute empty'''
-        self.first_name = ''
+    def __setattr__(self, name, value):
+        '''setting the value of the instance attribute'''
+        if name == 'first_name':
+            self.__dict__[name] = value
+        else:
+            raise AttributeError
+            ("'LockedClass' object has no attribute '{}'".format(name))
