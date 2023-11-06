f3690ef7b63afe81cc01daf5c35642cae311d302
ahmed <ahelmyeng1@gmail.com>
2023-11-06 06:00:19 -0800

    tasks

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''this is documentation for that module contains area method
+'''
+
+
+class BaseGeometry:
+    '''this is class for a regular method area'''
+
+    def area(self):
+        '''method that raises Exception message'''
+        raise Exception('area() is not implemented')
