2ef15d7c6c944d06b6de6d97ee8a503aef58b45b
ahmed <ahelmyeng1@gmail.com>
2023-11-06 05:36:59 -0800

    tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this is documentation for that module
+    contains function lookup
+'''
+
+
+def lookup(obj):
+    '''it gets list for attributes of an object'''
+    return dir(obj)
