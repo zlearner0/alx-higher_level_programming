a9c54854ae780069e29e59b3d84706f778c34978
ahmed <ahelmyeng1@gmail.com>
2023-11-06 05:56:11 -0800

    tasks

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this is documentation for that module
+    for a function check if an object is of the same class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''check object is directly from a class'''
+    return type(obj) is a_class
