28561e47849f2babb6a4ed05dda22422f2a45c51
ahmed <ahelmyeng1@gmail.com>
2023-11-06 06:07:33 -0800

    tasks

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''this is documentation for module contains class BaseGeometry has methods
+'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''contains square properties and methods'''
+
+    def __init__(self, size):
+        '''inistantiate the square object'''
+        self.integer_validator('width', size)
+        self.__size = size
+
+    def area(self):
+        '''update the area for the square'''
+        return self.__size**2
+
+    def __str__(self):
+        '''string representation for the square'''
+        s = f'[Square] {self.__size}/{self.__size}'
+        return s
