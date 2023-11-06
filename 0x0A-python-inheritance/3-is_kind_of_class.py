329237322ccad37517422d73a08399af98e264c7
ahmed <ahelmyeng1@gmail.com>
2023-11-06 05:56:53 -0800

    tasks

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this is documentation for that module
+    for a function of an object class or its bases
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''check object is from a class or its parents'''
+    return isinstance(obj, a_class)
