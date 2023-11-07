71a0ab53ce14e640ea7894cb458e506b9bfeeabd
ahmed <ahelmyeng1@gmail.com>
2023-11-07 06:04:46 -0800

    tasks

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''this module contains function to read a file'''
+
+
+def read_file(filename=""):
+    '''opens a file then read and print it'''
+    with open(filename, 'r') as rf:
+        print(rf.read())
