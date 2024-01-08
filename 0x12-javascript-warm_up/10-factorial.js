d8448dc241e008a67b99b7919c036a5315438b8d
ahmed <ahelmyeng1@gmail.com>
2024-01-08 13:05:28 -0800

    task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
-
-function factorial (f) {
-  if ((isNaN(f)) || (f === 1)) {
-    return 1;
-  } else {
-    return f * factorial(f - 1);
-  }
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
