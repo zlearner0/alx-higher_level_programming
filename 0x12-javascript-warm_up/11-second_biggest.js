f5c5b747d7baa1fb08d6119fca8e2d404dce71c6
ahmed <ahelmyeng1@gmail.com>
2024-01-08 13:12:33 -0800

    task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3)
-{
-console.log(0);
+if (process.argv.length <= 3) {
+  console.log(0);
 } else {
-const args = process.argv.map(Number)
-.slice(2, process.argv.length)
-.sort(function (x, y) {x - y});
-console.log(args[args.length - 2]);
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((i, j) => i - j);
+  console.log(args[args.length - 2]);
 }
