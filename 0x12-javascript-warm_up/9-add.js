99f91885cd00b8c42c3703e5e7e89c95c7b40d9c
ahmed <ahelmyeng1@gmail.com>
2024-01-08 12:55:01 -0800

    task

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,9 +3,8 @@
 const a = Number(process.argv[2]);
 const b = Number(process.argv[3]);
 
-function add (a, b)
-{
-console.log( a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 
-add (a, b);
+add(a, b);
